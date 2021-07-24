3b204bae35ce6c4ad2212a15e9ddeac67f5de0f8
qianyu <280624745@qq.com>
2021-07-24 17:22:09 +0800

    增加超时提示

--- main2.js
+++ main2.js
@@ -4,18 +4,82 @@ const delay = interval => {
   })
 }
 
-const promisePoller = options => {
-  const { fn, interval } = options
-  const poll = () => {
-    fn()
-    delay(interval).then(poll)
-  }
-  poll()
+const taskTimeout = (promise, timeout) => {
+  return new Promise((resolve, reject) => {
+    const timeId = setTimeout(() => {
+      reject('超时')
+    }, timeout)
+    promise.then(result => {
+      window.clearTimeout(timeId)
+      resolve(result)
+    })
+  })
 }
+
+const promisePoller = options => {
+  const { fn, interval, masterTimeout, timeout, shouldContinue } = options
+  let timeId
+
+  return new Promise((resolve, reject) => {
+    if (masterTimeout) {
+      timeId = setTimeout(() => {
+        reject('总超时')
+      }, masterTimeout)
+    }
+
+    const poll = () => {
+      let promiseFn = Promise.resolve(fn())
+      if (timeout) {
+        promiseFn = taskTimeout(promiseFn, timeout)
+      }
+      promiseFn
+        .then(result => {
+          if (shouldContinue(null, result)) {
+            //shouldContinue(error,result){}
+            delay(interval).then(poll)
+          } else {
+            if (timeId) {
+              window.clearTimeout(timeId)
+            }
+            resolve(result)
+          }
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+
+    poll()
+  })
+}
+
+let n = 0
 
 promisePoller({
   fn: () => {
-    console.log(1)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log(1)
+        n++
+        resolve(n)
+      }, 300)
+    })
   },
-  interval: 1000
-})
+  interval: 1000,
+  masterTimeout: 5000,
+  timeout: 500,
+  shouldContinue: (error, promiseFn) => {
+    if (error || n === 8) {
+      return false
+    } else {
+      return true
+    }
+  }
+}).then(
+  data => {
+    console.log(data)
+  },
+  error => {
+    console.log(error)
+  }
+)
